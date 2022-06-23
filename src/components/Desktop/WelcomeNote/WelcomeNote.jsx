0c43b23297337c33098337c400ae2e59da9c7296
pabloecarranza <63490339+pabloecarranza@users.noreply.github.com>
2022-06-23 00:16:22 -0300

    new Changes Note

--- src/components/Desktop/WelcomeNote/WelcomeNote.jsx
+++ src/components/Desktop/WelcomeNote/WelcomeNote.jsx
@@ -4,19 +4,17 @@ import Styles from "./WelcomeNote.module.css";
 import styled from "styled-components";
 import CityContext from "../../../context/useContext";
 
-  const FullSize = styled.div`
-    display: block;
-    position: absolute;
-    height: 80vh;
-    width: 100vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `;
+const FullSize = styled.div`
+  display: block;
+  position: absolute;
+  height: 80vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 function WelcomeNote({ show, setShow }) {
-
-  const { user } = useContext(CityContext)
-
+  const { user } = useContext(CityContext);
 
   return (
     <FullSize>
@@ -30,20 +28,28 @@ function WelcomeNote({ show, setShow }) {
         <div className={Styles.textArea}>
           <div className={Styles.text}>
             <span>Hola {user} !</span>
-            <span>
-              Bienvenido a mi Aplicacion de Clima 
-            </span>
-           
-            <span>
-              Simulando un escritorio del Sistema Operativo macOS Big Sur con un formulario controlado de ingreso para el nombre. Cuenta con una la aplicacion para la consulta de clima, en la que se puede visualizar el pronóstico actual y el de los próximos 5 dias de la cuidad en la que se encuentre mediante geolocalización, y a su vez un selector que permita cambiar entre 5 cuidades preseleccionadas y un buscador de cuidades. 
+            <span>Bienvenido a mi Aplicacion de Clima</span>
 
+            <span>
+              Simulando un escritorio del Sistema Operativo macOS Big Sur con un
+              formulario controlado de ingreso para el nombre. Cuenta con una la
+              aplicacion para la consulta de clima, en la que se puede
+              visualizar el pronóstico actual y el de los próximos 5 dias de la
+              cuidad en la que se encuentre mediante geolocalización, y a su vez
+              un selector que permita cambiar entre 5 cuidades preseleccionadas
+              y un buscador de cuidades.
+              <br /><br />
               Espero que te guste :)
               
-              Aquí puedes encontrar mas proyectos mios:
-              Github: 🐱 https://github.com/pabloecarranza
-
-              Ademas te dejo mi correo para que estemos en contacto: 
-              Mail: 📬pabloecarranza@gmail.com
+              <br /><br />
+              Aquí puedes encontrar mas proyectos mios: 
+              
+             <br /> 🐱<a href="https://github.com/pabloecarranza" target="_blank">  https://github.com/pabloecarranza</a>
+              
+              <br /><br />
+              Ademas te dejo mi correo para que estemos en contacto: <br />
+              📬<a href="mailto:pabloecarranza@gmail.com">pabloecarranza@gmail.com</a>
+              
             </span>
           </div>
         </div>
